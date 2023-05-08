b4dd9faf57e1d083840c08e3d3cfad341213d100
Alan Martins <alanmf.pro@gmail.com>
2023-05-08 16:38:14 -0300

    Permissions for JWT users

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,10 +1,13 @@
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.shortcuts import get_object_or_404
 from ..models import Recipe
 from ..serializers import RecipeSerializer, TagSerializer
+from ..permissions import IsOwner
 from tag.models import Tag
 
 
@@ -16,6 +19,7 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
+    permission_classes = [IsAuthenticatedOrReadOnly,]
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -26,6 +30,34 @@ class RecipeAPIv2ViewSet(ModelViewSet):
             qs = qs.filter(category_id=category_id)
 
         return qs
+
+    def get_object(self):
+        pk = self.kwargs.get("pk", "")
+        obj = get_object_or_404(
+            self.get_queryset(),
+            pk=pk,
+        )
+
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsOwner(),]
+
+        return super().get_permissions()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
 
 
 @api_view()
